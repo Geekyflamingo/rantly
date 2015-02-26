c16628781c664d62d7a8c56068ec151062f4897f
Becca <rebecca.vasil@gmail.com>
2015-02-26 11:14:52 -0700

    can add new rant

--- app/controllers/rants/new.js
+++ app/controllers/rants/new.js
@@ -1,20 +1,19 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Controller.extend({
-
   actions: {
-    newRant: function () {
-      var title = this.get('title');
-      var body = this.get('body');
+    newRant: function() {
+      var title = this.get('newTitle');
+      var body = this.get('newBody');
 
-      if (body && title) {
-        var rant = this.store.createRecord('rant', { title: title, body: body});
+      if (body && title && body.trim() && title.trim()) {
+        var rant = this.store.createRecord('rant', { title: title, body: body });
+        this.set('newTitle', '');
+        this.set('newBody', '');
         rant.save().then(function(){
-          this.set('title', '');
-          this.set('body','');
-          this.transitionToRoute('/');
+          this.transitionToRoute('rants');
         }.bind(this));
       }
-    },
+    }
   }
 });
