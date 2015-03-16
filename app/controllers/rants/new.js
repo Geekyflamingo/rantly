f423d5065114940190b568c2da5ce64aba77dc5f
Becca <rebecca.vasil@gmail.com>
2015-03-15 19:15:11 -0600

    validations and rant word count added

--- app/controllers/rants/new.js
+++ app/controllers/rants/new.js
@@ -5,15 +5,27 @@ export default Ember.Controller.extend({
     newRant: function() {
       var title = this.get('newTitle');
       var body = this.get('newBody');
+      var input = document.getElementsByClassName("errors")[0];
 
-      if (body && title && body.trim() && title.trim()) {
-        var rant = this.store.createRecord('rant', { title: title, body: body });
-        this.set('newTitle', '');
-        this.set('newBody', '');
-        rant.save().then(function(){
-          this.transitionToRoute('rants');
-        }.bind(this));
-      }
+      if ((!title) && ((!body) || (body.length < 143))) {
+        var error = document.createTextNode("Your rant must have a title. \n");
+        input.appendChild(error);
+        var errorBody = document.createTextNode("Please say more! Your rant must be at least 144 characters.");
+        input.appendChild(errorBody);
+      } else if ((body == null) || (body.length < 143)) {
+        var error = document.createTextNode("Please say more! Your rant must be at least 144 characters.");
+        input.appendChild(error);
+      } else if (title.length === 0) {
+        var error = document.createTextNode("Your rant must have a title.");
+        input.appendChild(error);
+      }else {
+          var rant = this.store.createRecord('rant', { title: title, body: body });
+          this.set('newTitle', '');
+          this.set('newBody', '');
+          rant.save().then(function(){
+            this.transitionToRoute('rants');
+          }.bind(this));
+        }
     },
     cancelRant: function() {
       this.transitionToRoute('rants');
