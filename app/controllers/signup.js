73106d07d100d7cf99a7ae4910072987927c1cf1
Becca <rebecca.vasil@gmail.com>
2015-03-23 10:49:47 -0600

    Signup error message now shows

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -9,25 +9,37 @@ export default Ember.ArrayController.extend({
       var controller = this;
       var firstName = this.get('fnameCopy'),
           lastName  = this.get('lnameCopy'),
-          email     = this.get('emailCopy').trim(),
+          email     = this.get('emailCopy'),
           password  = this.get('passwordCopy'),
           passwordConfirm  = this.get('passwordConfirm');
 
-
-      var user = controller.store.createRecord('user',
-      { firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: password,
-        passwordConfirmation: passwordConfirm});
-      user.save().then(function(){
-        controller.set('fnameCopy', '');
-        controller.set('lnameCopy', '');
-        controller.set('emailCopy', '');
-        controller.set('passwordCopy', '');
-        controller.set('passwordConfirm', '');
-        controller.transitionToRoute('rants');
-      });
-    }
+      if(
+        ((firstName == null) || (firstName.length < 0)) ||
+        ((lastName == null) || (lastName.length < 0)) ||
+        ((email == null) || (email.length < 0)) ||
+        ((password == null) || (password.length < 0)) ||
+        ((passwordConfirm == null) || (passwordConfirm.length < 0)) ||
+        (password !== passwordConfirm)
+      ){
+        var input = document.getElementsByClassName("errors")[0];
+        var error = document.createTextNode("All fields are required!");
+        input.appendChild(error);
+      }else{
+        var user = controller.store.createRecord('user',
+        { firstName: firstName,
+          lastName: lastName,
+          email: email,
+          password: password,
+          passwordConfirmation: passwordConfirm});
+        user.save().then(function(){
+          controller.set('fnameCopy', '');
+          controller.set('lnameCopy', '');
+          controller.set('emailCopy', '');
+          controller.set('passwordCopy', '');
+          controller.set('passwordConfirm', '');
+          controller.transitionToRoute('rants');
+        });
+      }
+    },
   }
 });
